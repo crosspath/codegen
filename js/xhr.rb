f7b9f148909a2128a4418aba667dcafa8e311cf1
Evgeniy Nochevnov <xpath@e1.ru>
2020-05-23 12:32:05 +0000

    Добавлен вызов сериализации как метод объекта/класса модели

--- js/xhr.rb
+++ js/xhr.rb
@@ -4,3 +4,30 @@ $main.run 'yarn add axios axios-rest-client'
 
 f('app/javascript/lib/api.js', 'js/api.js')
 f('app/javascript/lib/dom.js', 'js/dom.js')
+
+$main.inject_into_file(
+  'app/models/application_record.rb',
+  after: 'self.abstract_class = true'
+) do
+  <<-END
+
+  class << self
+    def serializer_name(class_name)
+      @_serializer_name = class_name
+    end
+
+    def serializer_class
+      @_serializer_name ||= "#{self.name}Serializer".freeze
+      @_serializer_name.constantize
+    end
+
+    def serialized(serializer: serializer_class, **options)
+      serializer.render_as_hash(self.all, **options)
+    end
+  end
+
+  def serialized(serializer: self.class.serializer_class, **options)
+    serializer.render_as_hash(self, **options)
+  end
+  END
+end
