d31cf665ae4a6c4d7cef665927de22745a456f85
Евгений Ночевнов <enochevnov@rzdit.ru>
2020-11-27 12:32:42 +0300

    Исправления для Capistrano

--- generators/capistrano.rb
+++ generators/capistrano.rb
@@ -1,22 +1,4 @@
-Generator.add_actions do |answers|
-  next unless answers[:capistrano]
-
-  $main.gem_group :development do
-    $main.gem 'capistrano'
-    $main.gem 'capistrano-bundler'
-    $main.gem 'capistrano-passenger'
-    $main.gem 'capistrano-rails'
-    $main.gem 'capistrano-rails-console'
-    $main.gem 'capistrano-rvm'
-    $main.gem 'capistrano-sidekiq', '2.0.0.beta5'
-    $main.gem 'capistrano-yarn'
-  end
-
-  # $main.group :development, :production do
-  #   # Для Sidekiq 6.
-  #   $main.gem 'listen', '>= 3.0.5', '< 3.2'
-  # end
-
+def capistrano_sidekiq(answers)
   product_name = answers[:product_name] || "Product #{Time.now.to_i.to_s(36)}"
   with_dashes  = product_name.gsub(' ', '-')
   service_name = "sidekiq-#{with_dashes}"
@@ -31,13 +13,7 @@ Generator.add_actions do |answers|
   )
 
   after_bundle_install do
-    $main.run 'bundle binstubs capistrano'
-    $main.run 'bin/cap install'
-
     requires = <<~END
-
-      require 'capistrano/data_migrate'
-      require 'capistrano/yarn'
       require 'capistrano/sidekiq'
 
       install_plugin Capistrano::Sidekiq
@@ -45,30 +21,9 @@ Generator.add_actions do |answers|
     END
     $main.append_to_file('Capfile', requires)
 
-    uncomment = [
-      'require "capistrano/rvm"',
-      'require "capistrano/bundler"',
-      'require "capistrano/rails/assets"',
-      'require "capistrano/rails/migrations"',
-      'require "capistrano/passenger"',
-    ]
-
-    replace_strings('Capfile', uncomment.map { |x| {from: "\# #{x}", to: x} })
-
-    deploy_text = <<~END
-    # Разворачивание на сервере
-
-        bin/cap production deploy
-    END
-
-    $main.append_to_file('README.md', deploy_text)
-
     deploy_conf = <<~END
-      set :rvm_ruby_version, '2.6.5'
-
-      set :passenger_restart_with_touch, true
-
-      set :sidekiq_service_unit_name, '#{service_name}'
+      set :init_system, :systemd
+      set :service_unit_name, '#{service_name}'
     END
 
     $main.inject_into_file(
@@ -76,36 +31,104 @@ Generator.add_actions do |answers|
       deploy_conf,
       before: 'set :application'
     )
-
-    $main.inject_into_file(
-      'config/deploy.rb',
-      "set :deploy_to, '#{path}'\n",
-      before: "\n# Default value for :format"
-    )
-
-    linked_files = <<~END
-      append :linked_files, 'config/database.yml'
-      append :linked_files, 'config/master.key'
-      append :linked_files, 'config/credentials.yml.enc'
-      append :linked_files, '.env.production.local'
-    END
-
-    $main.inject_into_file(
-      'config/deploy.rb',
-      linked_files,
-      before: '# append :linked_files'
-    )
-
-    linked_dirs = <<~END
-      append :linked_dirs, 'log', 'storage', 'tmp/pids', 'tmp/cache'
-      append :linked_dirs, 'tmp/sockets', 'public/system', 'node_modules'
-      append :linked_dirs, 'public/packs'
-    END
-
-    $main.inject_into_file(
-      'config/deploy.rb',
-      linked_dirs,
-      before: '# append :linked_dirs'
-    )
   end
+end
+
+def capistrano_after_bundle
+  $main.run 'bundle binstubs capistrano'
+  $main.run 'bin/cap install'
+
+  requires = <<~END
+
+    require 'capistrano/data_migrate'
+    require 'capistrano/yarn'
+  END
+  $main.append_to_file('Capfile', requires)
+
+  uncomment = [
+    'require "capistrano/rvm"',
+    'require "capistrano/bundler"',
+    'require "capistrano/rails/assets"',
+    'require "capistrano/rails/migrations"',
+    'require "capistrano/passenger"',
+  ]
+
+  replace_strings('Capfile', uncomment.map { |x| {from: "\# #{x}", to: x} })
+
+  deploy_text = <<~END
+  # Разворачивание на сервере
+
+      bin/cap production deploy
+  END
+
+  $main.append_to_file('README.md', deploy_text)
+
+  deploy_conf = <<~END
+    set :rvm_ruby_version, '2.6.5'
+
+    set :passenger_restart_with_touch, true
+  END
+
+  $main.inject_into_file(
+    'config/deploy.rb',
+    deploy_conf,
+    before: 'set :application'
+  )
+
+  $main.inject_into_file(
+    'config/deploy.rb',
+    "set :deploy_to, '#{path}'\n",
+    before: "\n# Default value for :format"
+  )
+
+  linked_files = <<~END
+    append :linked_files, 'config/database.yml'
+    append :linked_files, 'config/master.key'
+    append :linked_files, 'config/credentials.yml.enc'
+    append :linked_files, '.env.production.local'
+  END
+
+  $main.inject_into_file(
+    'config/deploy.rb',
+    linked_files,
+    before: '# append :linked_files'
+  )
+
+  linked_dirs = <<~END
+    append :linked_dirs, 'log', 'storage', 'tmp/pids', 'tmp/cache'
+    append :linked_dirs, 'tmp/sockets', 'public/system', 'node_modules'
+    append :linked_dirs, 'public/packs'
+  END
+
+  $main.inject_into_file(
+    'config/deploy.rb',
+    linked_dirs,
+    before: '# append :linked_dirs'
+  )
+end
+
+Generator.add_actions do |answers|
+  next unless answers[:capistrano]
+
+  $main.gem_group :development do
+    $main.gem 'capistrano'
+    $main.gem 'capistrano-bundler'
+    $main.gem 'capistrano-passenger'
+    $main.gem 'capistrano-rails'
+    $main.gem 'capistrano-rails-console'
+    $main.gem 'capistrano-rvm'
+    $main.gem 'capistrano-sidekiq', '2.0.0.beta5' if answers[:sidekiq]
+    $main.gem 'capistrano-yarn'
+  end
+
+  # $main.group :development, :production do
+  #   # Для Sidekiq 6.
+  #   $main.gem 'listen', '>= 3.0.5', '< 3.2'
+  # end
+
+  after_bundle_install do
+    capistrano_after_bundle
+  end
+
+  capistrano_sidekiq(answers) if answers[:sidekiq]
 end
