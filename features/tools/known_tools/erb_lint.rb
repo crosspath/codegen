10de07f7ad2393e5a84fcd5c25bcb80ddedb22db
Evgeniy Nochevnov <accounts@nochevnov.ru>
2024-04-03 19:53:50 +0300

    Add tools: ERB Lint, ESLint, Prettier

---
+++ features/tools/known_tools/erb_lint.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Features::Tools::KnownTools
+  class ERBLint < Features::Tools::KnownTool
+    register_as "ERB Lint", adds_config: true
+
+    def call(use_tools)
+      puts "Add ERB Lint..."
+
+      rubocop = use_tools[:rubocop]
+
+      erb("config/.erb-lint", File.join(DIR_CONFIG, ".erb-lint.yml"), rubocop:)
+      copy_files_to_project("bin/erblint", DIR_BIN)
+    end
+  end
+end
